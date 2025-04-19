0a9c0cb33306b002a89d892b9360bea799c8850b
Pelemeni52 <samarskiygleb@gmail.com>
2025-04-19 14:30:54 +1000

    Update main.c

--- lab6/main.c
+++ lab6/main.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    FILE *fp1, *fp2;
+    FILE *f1, *f2;
     
-    fp1 = fopen("input.txt", "r");
-    fp2 = fopen("output.txt", "w");
+    f1 = fopen("input.txt", "r");
+    f2 = fopen("output.txt", "w");
     char s1[100];
     
-    while(fgets(s1, 50, fp1)){
+    while(fgets(s1, 50, f1)){
         char s2[100];
         strncpy(s2, s1+strlen(s1)-5, strlen(s1));
         if(atoi(s2) > 1980)
-            fprintf(fp2, "%s", s1);
+            fprintf(f2, "%s", s1);
     }
-    fclose(fp1);
-    fclose(fp2);
+    fclose(f1);
+    fclose(f2);
 
     return 0;
 }
